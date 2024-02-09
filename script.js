1a7a62738d5a10f169bceeb44e4fb1c81bb4cce7
gobindosarkar11 <onlinetrust95@gmail.com>
2024-02-09 22:20:17 +0600

    problem solving

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-function calculateMoney(){
+function calculateMoney(ticketSell){
+    const ticketPrice = ticketSell * 120;
+    const darowanCost = 500;
+    const lunchCost = 8 * 50;
+    const totalStuffCost = darowanCost + lunchCost;
+    const totalMoney = ticketPrice - totalStuffCost;
+    return totalMoney;
+}
 
+
+function checkName(spell){
+    const lastCharecter = spell[spell.length -1];
+    if(lastCharecter === 'a' || lastCharecter === 'y' || lastCharecter === 'i' || lastCharecter === 'e' || lastCharecter === 'o' || lastCharecter === 'u' || lastCharecter === 'w' ){
+        return 'Good Name';
+    }
+    else{
+        return 'Bad Name';
+    }
+}
+
+
+function deleteInvalids(remove) {
+    return remove.filter(element => typeof element === 'number' && !isNaN(element));
 }
